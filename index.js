428b0f262ad6012431149f9fe86b8f09600c078a
Tuan Nguyen <trentontuannguyen@gmail.com>
2021-11-24 00:33:28 -0500

    commit tree 85a76ed0fe37ac9d5e9a8ef681dfdae4e232cc65

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
-}
+function saturdayFun(activity = "roller-skate") {
+  return  `This Saturday, I want to ${activity}!`;
+} //function declaration
 
-let mondayWork = function(activity="go to the office") {
+let mondayWork = function (activity = "go to the office") {
   return `This Monday, I will ${activity}.`
-}
+} //function expression = declare a var and assign a fn as its value
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+function wrapAdjective(wrap = "*") {
+  return function (adjective = "special") {
+    return `You are ${wrap}${adjective}${wrap}!`;
   }
 }
+
+//wrapAdjective("%")("a dedicate d programmer");
